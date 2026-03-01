fe788d572539a686ff86bf589c84155049455fca
Ritesh <riteshswain1973@gmail.com>
2026-03-01 15:42:00 +0530

    How to declare variable

---
+++ Shell_Scripting/variable.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+<< 'comment'
+Multi
+Line
+Comment
+comment
+
+name="Ritesh"
+echo "My name is $name and date is $(date)"
+
+# For variable use dollar $ and for command use $(date)
+
+
